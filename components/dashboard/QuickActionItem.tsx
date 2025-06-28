eb438d0a334e01eb81958678b24cb09e14d67d98
Gian Mathew S. Gutang <125986765+MatSollano@users.noreply.github.com>
2025-06-28 19:12:33 +0800

    Updated QuickActionItem.tsx

--- components/dashboard/QuickActionItem.tsx
+++ components/dashboard/QuickActionItem.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { Flask, QrCode } from 'lucide-react-native';
+import { FlaskRound as Flask, QrCode } from 'lucide-react-native';
 
 interface QuickActionItemProps {
   icon: string;
